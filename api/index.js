753c847c3f1b91bd212ff74e2e6ba07ed60c34de
Tom Humphrey <tomhu@brandwatch.com>
2019-10-16 12:20:34 +0100

    Added content-type

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express()
 const fs = require('fs')
 const port = 3000
 
+// loading routes, to tell express that when someone hits one of these, we want the attached callback to run
 app.get('/', (req, res) => res.send('Hello World'))
 app.get('/hello', (req, res) => res.send('Hello World 3'))
 app.get('/records', (req, res) => {
@@ -10,6 +11,7 @@ app.get('/records', (req, res) => {
     if (err) {
       return res.send(500)
     }
+    res.setHeader('Content-Type', 'application/json')
     res.send(buffer)
   })
 })
