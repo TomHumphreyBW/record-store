8a364d31f149e4e5600f88ccc2d787ca602ea14f
Tom Humphrey <tomhu@brandwatch.com>
2020-01-28 11:40:06 +0000

    Searchbar and sorting working

--- frontend/src/Searchbar.js
+++ frontend/src/Searchbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Searchbar.css';
 import SortDropdown from './SortDropdown.js';
 import {
+    Icon,
     TextInput,
     TextInputIcon,
     TextInputButton,
@@ -11,39 +12,48 @@ import {
 
 const Searchbar = ({ setSearch, setSort, sort }) => {
     const [innerSearch, setInnerSearch] = useState('')
+    const [open, setOpen] = useState(false);
     return (
         <div className="Searchbar">
-            <form
-                onSubmit={(event) => {
-                    event.preventDefault();
-                    setSearch(innerSearch)
+            <Icon name="hamburger" size="2rem" color="white"
+                onClick={(event) => {
+                    setOpen(state => !state)
                 }}
-            >
-                <TextInput
-                    length="short"
-                    onChange={(event) => {
-                        setInnerSearch(event.target.value)
-                    }}
-                    placeholder="Search albums"
-                >
-                    <TextInputIcon align="left" name="magnify-glass"
-                        tooltip={
-                            <TooltipContext color="carbon">
-                                <TooltipContent>
-                                    Search for an album
-                                </TooltipContent>
-                            </TooltipContext>
-                        }
-                    />
-                    <TextInputButton color="positive">
-                        Search
-                    </TextInputButton>
-                </TextInput>
-            </form>
-            <SortDropdown
-                setSort={setSort}
-                sort={sort}
             />
+            {open && <React.Fragment>
+                <form
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                        setSearch(innerSearch)
+                    }}
+                >
+                    <TextInput
+                        length="short"
+                        onChange={(event) => {
+                            setInnerSearch(event.target.value)
+                        }}
+                        placeholder="Search albums"
+                    >
+                        <TextInputIcon align="left" name="magnify-glass"
+                            tooltip={
+                                <TooltipContext color="carbon">
+                                    <TooltipContent>
+                                        Search for an album
+                                </TooltipContent>
+                                </TooltipContext>
+                            }
+                        />
+                        <TextInputButton color="positive">
+                            Search
+                    </TextInputButton>
+                    </TextInput>
+                </form>
+                <SortDropdown
+                    setSort={setSort}
+                    sort={sort}
+                />
+            </React.Fragment>
+            }
         </div>
     );
 }
